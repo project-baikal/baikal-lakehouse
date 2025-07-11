bd659f17bd35bbcaa5968b045ba75436d059bcbf
VictoriaCardwell <96270069+VictoriaCardwell@users.noreply.github.com>
2025-07-10 22:45:15 -0400

    working asset of poc_data

--- pipelines/poc_data/poc_data_dagster/poc_data_dagster/assets.py
+++ pipelines/poc_data/poc_data_dagster/poc_data_dagster/assets.py
@@ -1,1 +1,37 @@
+import requests
+import json
+import boto3
+from dagster import asset, Output, MetadataValue
+import os
 
+s3 = boto3.client("s3")
+BUCKET_NAME = 'project-baikal'
+PREFIX = "poc_data/bronze/country/"
+
+COUNTRIES = ["canada", "mexico", "china", "japan", "costa"]
+
+@asset
+def country_data_to_s3() -> Output[list]:
+    results = []
+    for country in COUNTRIES:
+        url = f"https://restcountries.com/v3.1/name/{country}"
+        resp = requests.get(url)
+        resp.raise_for_status()
+
+        data = resp.json()[0]
+        key = f"{PREFIX}{country}.json"
+        s3.put_object(
+            Bucket=BUCKET_NAME,
+            Key=key,
+            Body=json.dumps(data),
+            ContentType="application/json"
+        )
+        results.append({"country": country, "s3_key": key})
+
+    return Output(
+        value=results,
+        metadata={
+            "countries_fetched": len(results),
+            "s3_prefix": MetadataValue.path(f"s3://{BUCKET_NAME}/{PREFIX}"),
+        }
+    )
